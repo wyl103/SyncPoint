79b862bf35e4ad5fe9c21d7de3dffd804926c24f
Wilman Arias <wilman@srv1325465.hstgr.cloud>
2026-08-12 16:12:28 +0000

    fix: cambios completos

--- app/services/Database.php
+++ app/services/Database.php
@@ -15,7 +15,7 @@ class Database {
             $pass = $env['DB_PASS'];
             $charset = 'utf8mb4';
 
-            $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+            $dsn = "pgsql:host=$host;port=5432;dbname=$db;";
             $options = [
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
